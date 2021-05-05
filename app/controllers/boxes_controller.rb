7bc4b84dfec1d261b000dc6346ec550bfeab772a
SneyderHOL <1507@holbertonschool.com>
2021-05-05 00:37:44 -0500

    Update boxes controller and views

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,10 +1,12 @@
 class BoxesController < ApplicationController
+  before_action :validate_user_belongs_to_account, only: [:show]
+  
   def index
     @boxes = Box.all
   end
 
   def show
-    @box = Box.find_by(id: params[:id])
+    @box = Box.find(params[:id])
   end
 
   def new
@@ -12,12 +14,25 @@ class BoxesController < ApplicationController
   end
 
   def create
-    @box = Box.new(box_params)
+    @box = Box.new(box_params.merge(account: current_account))
+    if @box.save
+      flash[:notice] = "Box was created successfully."
+      redirect_to @box
+    else
+      render 'new'
+    end
   end
 
   private
 
   def box_params
     params.require(:box).permit(:name)
+  end
+
+  def validate_user_belongs_to_account
+    unless current_account.users.include?(current_user)
+      flash[:alert] = "You don't have permissions to perform that action"
+      redirect_to root_path
+    end
   end
 end
