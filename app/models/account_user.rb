3cb3ff3b0ff27bb7657be837143826ac80be0eb3
SneyderHOL <1507@holbertonschool.com>
2021-05-10 15:15:25 -0500

    Add validations for account_user

--- app/models/account_user.rb
+++ app/models/account_user.rb
@@ -2,6 +2,10 @@ class AccountUser < ApplicationRecord
   after_initialize :default_values
   belongs_to :account
   belongs_to :user
+  validates_inclusion_of :admin, in: [true, false],
+                          message: "must be true or false"
+  validates_uniqueness_of :user, :scope => :account,
+                          message: "has already been associated"
   
   private
 
