77774d104fea5f0159a81f35b4b3aebbf98a4b63
SneyderHOL <1507@holbertonschool.com>
2021-05-10 17:33:46 -0500

    Add test case for price

--- spec/factories/prices.rb
+++ spec/factories/prices.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :price do
-    currency { "MyString" }
-    amount { 1 }
-    interval { "MyString" }
-    stripe_price_id { "MyString" }
-    plan { nil }
+    currency { "usd" }
+    amount { 1000 }
+    interval { "Month" }
+    sequence(:stripe_price_id) { |n| "PrKySqmfgx98l1a#{n}ng" }
+    plan { create(:plan) }
   end
 end
--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Price, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    let(:price) { build(:price) }
+    
+    it 'test that factory object is valid' do
+      expect(price).to be_valid
+    end
+
+    it 'test that factory sequence is valid' do
+      price.save
+      second_price = build(:price)
+      expect(second_price).to be_valid
+    end
+
+    it 'test for uniquenes' do
+      aggregate_failures do
+        price.save
+        second_price = build(
+          :price,
+          plan: price.plan,
+          stripe_price_id: price.stripe_price_id
+        )
+        expect(second_price).not_to be_valid
+        expect(second_price.errors[:plan]).to(
+          include("has already been associated")
+        )
+        expect(second_price.errors[:stripe_price_id]).to(
+          include("has already been taken")
+        )
+      end
+    end
+
+    it 'test that has no empty fields' do
+      aggregate_failures do
+        price.currency = ""
+        price.amount = nil
+        price.interval = ""
+        price.stripe_price_id = ""
+        price.plan = nil
+        expect(price).not_to be_valid
+        expect(price.errors[:currency]).to include("can't be blank")
+        expect(price.errors[:amount]).to include("can't be blank")
+        expect(price.errors[:interval]).to include("can't be blank")
+        expect(price.errors[:stripe_price_id]).to include("can't be blank")
+        expect(price.errors[:plan]).to include("must exist")
+      end
+    end
+  end
 end
