8e3f6f48df6ba46112c950c73d49e7bbda4711ee
SneyderHOL <1507@holbertonschool.com>
2021-05-10 14:24:39 -0500

    Add validations to account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,9 @@
 class Account < ApplicationRecord
   attr_accessor :plan
 
-  validates :name, presence: true
+  validates :name, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 95 }
   
   has_one :subscription, dependent: :destroy
   has_many :account_users, dependent: :destroy
