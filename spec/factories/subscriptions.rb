928f3df97af9bdd2a2279c1048de30fa207aa02c
SneyderHOL <1507@holbertonschool.com>
2021-05-10 17:52:00 -0500

    Add test case for subscription

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :subscription do
     active { false }
-    account { nil }
-    price { nil }
+    account { create(:account) }
+    price { create(:price) }
+    sequence(:stripe_subscription_id) { |n| "SbKySqmfgx98l1a#{n}ng" }
   end
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    let(:subscription) { build(:subscription) }
+    
+    it 'test that factory object is valid' do
+      expect(subscription).to be_valid
+    end
+
+    it 'test that factory sequence is valid' do
+      subscription.save
+      second_subscription = build(:subscription)
+      expect(second_subscription).to be_valid
+    end
+
+    it 'test for uniquenes' do
+      aggregate_failures do
+        subscription.save
+        second_subscription = build(
+          :subscription,
+          account: subscription.account,
+          stripe_subscription_id: subscription.stripe_subscription_id
+        )
+        expect(second_subscription).not_to be_valid
+        expect(second_subscription.errors[:account]).to(
+          include("has already been associated")
+        )
+        expect(second_subscription.errors[:stripe_subscription_id]).to(
+          include("has already been taken")
+        )
+      end
+    end
+
+    it 'test that has no empty fields' do
+      aggregate_failures do
+        subscription.price = nil
+        subscription.account = nil
+        subscription.active = nil
+        expect(subscription).not_to be_valid
+        expect(subscription.errors[:active]).to include("must be true or false")
+        expect(subscription.errors[:account]).to include("must exist")
+        expect(subscription.errors[:price]).to include("must exist")
+      end
+    end
+  end
 end
