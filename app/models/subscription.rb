e0c21add400445113a6c3e0e3f59ee403253e1af
SneyderHOL <1507@holbertonschool.com>
2021-05-10 17:52:17 -0500

    Add validations for subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,7 +2,11 @@ class Subscription < ApplicationRecord
   after_initialize :default_values
   acts_as_tenant(:account)
   belongs_to :price
-  
+  validates_inclusion_of :active, in: [true, false],
+                          message: "must be true or false"
+  validates_uniqueness_of :account, message: "has already been associated"
+  validates :stripe_subscription_id, presence: true,
+            uniqueness: { case_sensitive: false }
   private
 
   def default_values
