610e8a20267eca6fa9bbb65a0cd98be1b2452c18
SneyderHOL <1507@holbertonschool.com>
2021-05-12 11:58:21 -0500

    Update box model validation

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,13 +1,24 @@
 class Box < ApplicationRecord
   acts_as_tenant(:account)
   has_many :items
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 95 }
+  validate :name_uniqueness_within_same_account
   validate :limit_boxes_according_to_plan
 
   def limit_boxes_according_to_plan
-    limit = self.account.subscription.price.plan.box_limit
-    if limit > 0 && self.new_record? && (self.account.boxes.count == limit)
-      errors.add(:base, "Your account cannot have more boxes than #{limit}")
+    if self.account.subscription
+      limit = self.account.subscription.price.plan.box_limit
+      if limit > 0 && self.new_record? && (self.account.boxes.count >= limit)
+        errors.add(:base, "Your account cannot have more boxes than #{limit}")
+      end
+    else
+      errors.add(:base, "Your account cannot create boxes")
+    end
+  end
+
+  def name_uniqueness_within_same_account
+    if self.account.boxes.where('lower(name) = ?', name.downcase).first
+      errors.add(:name, "has already been taken")
     end
   end
 end
