dcca5891562f778848a9b25bf7b3c709df0fc2ba
SneyderHOL <1507@holbertonschool.com>
2021-05-12 16:58:52 -0500

    Add test case for item

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
   factory :item do
-    description { "MyString" }
-    box { nil }
+    description { "MyDescription" }
+    box { create(:box_within_free_account) }
     user { nil }
+    after (:build) do |item|
+      item.picture.attach(io: File.open(Rails.root.join('spec', 'fixtures', 'files', 'testing_picture.png')),
+                              filename: 'testing_picture.png', content_type: 'image/png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    let(:item) { build(:item) }
+        
+    it 'test for new factory object is valid' do
+      expect(item).to be_valid
+    end
+
+    it 'test that has no empty fields' do
+      aggregate_failures do
+        item.description = ""
+        item.picture = nil
+        expect(item).not_to be_valid
+        expect(item.errors[:description]).to include("can't be blank")
+        expect(item.errors[:picture]).to include("needs to be attached")
+      end
+    end
+
+    it 'test that import_file has a valid attached picture' do
+      aggregate_failures do
+        expect(item.picture).to be_attached
+        expect(item).to be_valid
+      end
+    end
+
+    it 'test that check invalid type attached picture' do
+      item.picture.attach(io: File.open(Rails.root.join('spec', 'fixtures', 'files', 'testing_file.csv')),
+                              filename: 'testing_file.csv')
+      expect(item).not_to be_valid
+    end
+  end
 end
