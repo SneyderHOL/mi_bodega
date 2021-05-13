5b40e4a5014cdf05d860ae6d5cbeb6d844e4557a
SneyderHOL <1507@holbertonschool.com>
2021-05-13 10:51:21 -0500

    Update account route

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
+
   root 'home#index'
-  get 'accounts', to: 'accounts#index', as: 'accounts'
-  put 'accounts/:id', to: 'accounts#update', as: 'update_account'
-  get 'add_users', to: 'users#new', as: 'add_new_user'
-  post 'add_users', to: 'users#create', as: 'create_new_user'
-  get 'billing_portal', to: 'billing_portal#new', as: 'billing_portal'
+
+  resources :accounts, only: [:index, :update]
+  
   resources :boxes, except: [:edit, :update, :destroy] do
     member do
       get 'qr_code', to: 'boxes#show_code'
     end
+    
     resources :items, except: [:index, :destroy] do
       member do
         post 'use_item'
       end
     end
   end
+
+  get 'add_users', to: 'users#new', as: 'add_new_user'
+  post 'add_users', to: 'users#create', as: 'create_new_user'
+
+  get 'billing_portal', to: 'billing_portal#new', as: 'billing_portal'
 end
