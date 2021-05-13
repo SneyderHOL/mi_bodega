851d3e6b7e3624ce26247eabefb88af202f896af
SneyderHOL <1507@holbertonschool.com>
2021-05-13 11:07:33 -0500

    Add test case for sessions

---
+++ spec/routing/session_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe RegistrationsController, type: :routing do
+  describe "routing" do
+    it 'should route to devise/sessions#new' do
+      expect(get: 'users/sign_in').to route_to('devise/sessions#new')
+    end
+
+    it 'should route to devise/sessions#create' do
+      expect(post: 'users/sign_in').to route_to('devise/sessions#create')
+    end
+
+    it 'should route to devise/sessions#destroy' do
+      expect(delete: 'users/sign_out').to route_to('devise/sessions#destroy')
+    end
+  end
+end
