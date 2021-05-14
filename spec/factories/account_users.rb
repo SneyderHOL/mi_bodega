11ec0bc1afd06cc8860b3245d29b462cbe4ccfea
SneyderHOL <1507@holbertonschool.com>
2021-05-14 11:54:47 -0500

    Update account_user test case

--- spec/factories/account_users.rb
+++ spec/factories/account_users.rb
@@ -1,7 +1,46 @@
 FactoryBot.define do
   factory :account_user do
-    admin { false }
-    account { nil }
-    user { nil }
+    factory :account_user_as_admin_with_free_account do
+      admin { true }
+      account { create(:account, :with_free_subscription) }
+      user { 
+        admin_user = create(:user)
+        create(:payment, user: admin_user)
+        admin_user
+      }
+    end
+    factory :account_user_as_admin_with_moderate_account do
+      admin { true }
+      account { create(:account, :with_moderate_subscription) }
+      user { 
+        admin_user = create(:user)
+        create(:payment, user: admin_user)
+        admin_user
+      }
+    end
+    factory :account_user_as_admin_with_unlimited_account do
+      admin { true }
+      account { create(:account, :with_unlimited_subscription) }
+      user { 
+        admin_user = create(:user)
+        create(:payment, user: admin_user)
+        admin_user
+      }
+    end
+    factory :account_user_as_not_admin_with_free_account do
+      admin { true }
+      account { create(:account, :with_free_subscription) }
+      user { create(:user) }
+    end
+    factory :account_user_as_not_admin_with_moderate_account do
+      admin { true }
+      account { create(:account, :with_moderate_subscription) }
+      user { create(:user) }
+    end
+    factory :account_user_as_not_admin_with_unlimited_account do
+      admin { true }
+      account { create(:account, :with_unlimited_subscription) }
+      user { create(:user) }
+    end
   end
 end
--- spec/models/account_user_spec.rb
+++ spec/models/account_user_spec.rb
@@ -2,30 +2,67 @@ require 'rails_helper'
 
 RSpec.describe AccountUser, type: :model do
   describe '#validations' do
-    let(:new_account_user) { build(:account_user) }
+    let(:admin_user_with_free_account) {
+      build(:account_user_as_admin_with_free_account)
+    }
+    let(:admin_user_with_moderate_account) {
+      build(:account_user_as_admin_with_moderate_account)
+    }
+    let(:admin_user_with_unlimited_account) {
+      build(:account_user_as_admin_with_unlimited_account)
+    }
+    let(:invited_user_with_free_account) {
+      build(:account_user_as_not_admin_with_free_account)
+    }
+    let(:invited_user_with_moderate_account) {
+      build(:account_user_as_not_admin_with_moderate_account)
+    }
+    let(:invited_user_with_unlimited_account) {
+      build(:account_user_as_not_admin_with_unlimited_account)
+    }
     
+    it 'test that factory objects are valid' do
+      aggregate_failures do
+        expect(admin_user_with_free_account).to be_valid
+        expect(admin_user_with_free_account.).to be_valid
+        expect(admin_user_with_moderate_account).to be_valid
+        expect(admin_user_with_unlimited_account).to be_valid
+        expect(invited_user_with_free_account).to be_valid
+        expect(invited_user_with_moderate_account).to be_valid
+        expect(invited_user_with_unlimited_account).to be_valid
+      end
+    end
+
     it 'test that fields are valid' do
       aggregate_failures do
-        new_account_user.admin = nil
-        expect(new_account_user).not_to be_valid
-        expect(new_account_user.errors[:admin]).to include("must be true or false")
-        expect(new_account_user.errors[:user]).to include("must exist")
-        expect(new_account_user.errors[:account]).to include("must exist")
+        admin_user_with_free_account.admin = nil
+        admin_user_with_free_account.user = nil
+        admin_user_with_free_account.account = nil
+        expect(admin_user_with_free_account).not_to be_valid
+        expect(admin_user_with_free_account.errors[:admin]).to(
+          include("must be true or false")
+        )
+        expect(admin_user_with_free_account.errors[:user]).to(
+          include("must exist")
+        )
+        expect(admin_user_with_free_account.errors[:account]).to(
+          include("must exist")
+        )
       end
     end
 
     it 'test that has no repeated user in same project' do
       aggregate_failures do
-        user = create(:user)
-        account = create(:account)
-        old_account_user = build(:account_user)
-        old_account_user.user = user
-        old_account_user.account = account
-        old_account_user.save
-        new_account_user.account = account
-        new_account_user.user = user
-        expect(new_account_user).not_to be_valid
-        expect(new_account_user.errors[:user]).to include("has already been associated")
+        invited_user_with_free_account.save
+        new_invited_user_with_free_account = build(
+          :account_user_as_not_admin_with_free_account,
+          account: invited_user_with_free_account.account,
+          user: invited_user_with_free_account.user
+        )
+        expect(new_invited_user_with_free_account).not_to be_valid
+        expect(new_invited_user_with_free_account.errors[:user]).to(
+          include("has already been associated")
+        )
       end
     end
   end
