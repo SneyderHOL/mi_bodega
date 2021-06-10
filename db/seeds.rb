4f38b9ab4b410545378c5315523244b7fe49e796
SneyderHOL <1507@holbertonschool.com>
2021-06-10 10:49:06 -0500

    Use find_or_create_by method

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-free_plan = Plan.create(name: "Free", box_limit: 1, stripe_product_id: "prod_JQNMf5VXOsJQRv")
-moderate_plan = Plan.create(name: "Moderate", box_limit: 5, stripe_product_id: "prod_JOlsk2Up4koGKV")
-unlimited_plan = Plan.create(name: "Unlimited", box_limit: 0, stripe_product_id: "prod_JOlt7J8RjVQ6pW")
+free_plan = Plan.find_or_create_by(name: "Free", box_limit: 1, stripe_product_id: "prod_JQNMf5VXOsJQRv")
+moderate_plan = Plan.find_or_create_by(name: "Moderate", box_limit: 5, stripe_product_id: "prod_JOlsk2Up4koGKV")
+unlimited_plan = Plan.find_or_create_by(name: "Unlimited", box_limit: 0, stripe_product_id: "prod_JOlt7J8RjVQ6pW")
 
-Price.create(currency: "usd", amount: 0, interval: "month", stripe_price_id: "price_1InWgGDRun2Qs8KlsqUzbY8J", plan:free_plan)
-Price.create(currency: "usd", amount: 2500, interval: "month", stripe_price_id: "price_1IlyavDRun2Qs8KlSLZUQgkc", plan:moderate_plan)
-Price.create(currency: "usd", amount: 5000, interval: "month", stripe_price_id: "price_1IlybsDRun2Qs8KlCKwMRR8z", plan:unlimited_plan)
+Price.find_or_create_by(currency: "usd", amount: 0, interval: "month", stripe_price_id: "price_1InWgGDRun2Qs8KlsqUzbY8J", plan:free_plan)
+Price.find_or_create_by(currency: "usd", amount: 2500, interval: "month", stripe_price_id: "price_1IlyavDRun2Qs8KlSLZUQgkc", plan:moderate_plan)
+Price.find_or_create_by(currency: "usd", amount: 5000, interval: "month", stripe_price_id: "price_1IlybsDRun2Qs8KlCKwMRR8z", plan:unlimited_plan)
